dbd4f1ec32fcc24e1d96f380be4d786648087dda
rabahdouasii@yahoo.fr <rabahdouassi@yahoo.fr>
2026-06-09 15:46:08 +0200

    Ton message de mise à jour

--- database/migrations/2024_11_25_224449_create_tassili_cruds_table.php
+++ database/migrations/2024_11_25_224449_create_tassili_cruds_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('hoggarcruds');
+        Schema::dropIfExists('tassili_cruds');
     }
 };
